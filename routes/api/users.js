0e07fc8bc8ac1c8a579e8add2b283aaf808f6cc7
unknown <zoka_cs_pro@hotmail.com>
2020-10-23 00:18:29 +0200

    Created the user model, added user registration post request and validation using express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route GET api/users
-// @desc Test route
+// @route POST api/users
+// @desc Register user
 // @access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  // Validate data using express validators
+  [check('name', 'Name is required').not().isEmpty()],
+  [check('email', 'Please enter a valid email').isEmail()],
+  [
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    // If there are errors, send 400 bad request and a list of errors using errors.array() function
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const app = express();
 // Connect database
 connectDB();
 
+// Initialize middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
