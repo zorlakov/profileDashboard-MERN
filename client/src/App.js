c098c060e6c8002c8b832f221b44e41730a92911
unknown <zoka_cs_pro@hotmail.com>
2020-10-27 16:36:13 +0100

    Added react router with rotues to landing page, login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Fragment } from 'react';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Register = () => {
+  return <div>Reg</div>;
+};
+
+export default Register;
