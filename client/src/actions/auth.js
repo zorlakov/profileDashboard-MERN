c2df26eb65e71632576af9607ee77e34814669a8
unknown <zoka_cs_pro@hotmail.com>
2020-10-29 17:47:33 +0100

    Implemented changing options in navbar based on whether user is logged in(authenticated) or not

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 import { setAlert } from './alert';
 import { setAuthToken } from '../utils/setAuthToken';
@@ -82,4 +83,11 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout
+export const logout = () => (dispatch) => {
+  dispatch({
+    type: LOGOUT,
+  });
 };
