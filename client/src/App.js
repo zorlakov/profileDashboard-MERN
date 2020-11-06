876c66064c408f2632184414fd6828bcbfe1aa62
unknown <zoka_cs_pro@hotmail.com>
2020-11-06 11:38:55 +0100

    Created and implemented the edit profile component

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/Routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 // Redux imports
 import { Provider } from 'react-redux';
 import store from '../src/store';
@@ -40,6 +41,11 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
               />
             </Switch>
           </section>
