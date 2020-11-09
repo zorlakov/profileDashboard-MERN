0b339035c4efbd33b3388637c9beb4a24f93341a
unknown <zoka_cs_pro@hotmail.com>
2020-11-09 17:05:02 +0100

    implemented displaying github repos in view user profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({
   match,
@@ -68,6 +69,9 @@ const Profile = ({
                 <h4>No education</h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
